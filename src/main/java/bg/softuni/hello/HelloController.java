c39e258016dc293a697676dea374f1d97f620e1a
truetranslator <55185307+truetranslator@users.noreply.github.com>
2020-09-09 19:33:03 +0300

    simple hello rest API

---
+++ src/main/java/bg/softuni/hello/HelloController.java
@@ -0,0 +1,13 @@
+package bg.softuni.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
